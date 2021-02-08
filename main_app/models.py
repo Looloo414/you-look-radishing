816650b564f1a5c887ce696a4a920fb9dbb5574b
Looloo414 <alexafaulkison@gmail.com>
2021-02-07 22:52:11 -0700

    Base, Home, About

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# MODELS 
+
+class Workout: 
+    def __init__(self, activity, howLong, description, time ):
+        self.activity = activity
+        self.howLong = howLong
+        self.description = description
+        self.time = time 
+
+workouts = [
+    Workout('Stretch', 5, 'Loosen Muscles', '5:30')
+]
+
--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse(<h1>You look radishing</h1>)
+    return HttpResponse('<h1>You look radishing</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def workout_index(request):
+    return render(request, 'workout/index.html', {'workouts': workouts})
