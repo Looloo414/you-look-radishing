b50132e968501d2e3d06d9c032621dba8491b037
Looloo414 <alexafaulkison@gmail.com>
2021-02-07 23:38:34 -0700

    Index Workouts

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # MODELS 
 
-class Workout: 
-    def __init__(self, activity, howLong, description, time ):
-        self.activity = activity
-        self.howLong = howLong
-        self.description = description
-        self.time = time 
+# class Workout: 
+#     def __init__(self, activity, howLong, description, time ):
+#         self.activity = activity
+#         self.howLong = howLong
+#         self.description = description
+#         self.time = time 
 
-workouts = [
-    Workout('Stretch', 5, 'Loosen Muscles', '5:30')
-]
+# workouts = [
+#     Workout('Stretch', 5, 'Loosen Muscles', '5:30'),
+#     Workout('Pushups', 20, 'Push body up and down', '6:30')
+# ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Workout
 
+class Workout: 
+    def __init__(self, activity, howLong, description, time ):
+        self.activity = activity
+        self.howLong = howLong
+        self.description = description
+        self.time = time 
+
+workouts = [
+    Workout('Stretch', 5, 'Loosen Muscles', '5:30'),
+    Workout('Pushups', 20, 'Push body up and down', '6:30')
+]
 
 def home(request):
     return render(request, 'home.html')
@@ -10,4 +20,4 @@ def about(request):
     return render(request, 'about.html')
 
 def workouts_index(request):
-  return render(request, 'workouts/index.html', { 'workouts': workout })
+  return render(request, 'workouts/index.html', { 'workouts': workouts })
