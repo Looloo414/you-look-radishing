c4eca23e67c40ddb4d69c58242c5c11d7dc71bc6
Looloo414 <alexafaulkison@gmail.com>
2021-02-07 23:07:29 -0700

    Main Workout Model

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('workout/', views.workout_index, name='index'),
+    path('workout/', views.workouts_index, name='index'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Workout
 
-# Create your views here.
+
 def home(request):
-    return HttpResponse('<h1>You look radishing</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def workout_index(request):
-    return render(request, 'workout/index.html', {'workouts': workouts})
+def workouts_index(request):
+  return render(request, 'workouts/index.html', { 'workouts': workout })
